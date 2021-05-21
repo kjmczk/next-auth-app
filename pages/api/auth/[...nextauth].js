147f361f3a519a29cab6bb929436ae3303e35f51
kjmczk <vollgong@gmail.com>
2021-05-21 19:06:37 +0900

    Remove some due to upgrading `next-auth`

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,7 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
-const options = {
-  site: process.env.SITE || 'http://localhost:3000',
-
+export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.Google({
@@ -30,6 +28,4 @@ const options = {
 
   // A database is optional, but required to persist accounts in a database
   database: process.env.DATABASE_URL,
-};
-
-export default (req, res) => NextAuth(req, res, options);
+});
